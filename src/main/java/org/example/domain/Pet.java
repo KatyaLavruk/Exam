b465a505e02efc0186b187a0f23897e1da662a97
Kateryna Lavruk <kkatyalavruk@gmail.com>
2026-04-28 09:58:50 +0300

    3

--- src/main/java/org/example/domain/Pet.java
+++ src/main/java/org/example/domain/Pet.java
@@ -2,6 +2,22 @@ package org.example.domain;
 
 public class Pet {
 
+    private String name;
+    private int age;
 
+    private String getName(){
+        this.name = name;
+        return "";
+    }
+    private int getAge(){
+        this.age = age;
+        return 0;
+    }
+    private void setName(){
+
+    }
+    private void setAge(){
+
+    }
 
 }
