a313324b96dbb15b9019d897de7a8c0c941313b2
Kateryna Lavruk <kkatyalavruk@gmail.com>
2026-04-28 09:57:11 +0300

    2

--- src/main/java/org/example/domain/Adopter.java
+++ src/main/java/org/example/domain/Adopter.java
@@ -2,22 +2,4 @@ package org.example.domain;
 
 public class Adopter extends Person{
 
-    private String name;
-    private int age;
-
-    private void setName(){
-
-    }
-    private void setAge(){
-
-    }
-    private String getName(){
-        this.name = name;
-        return "";
-    }
-    private int getAge(){
-        this.age = age;
-        return 0;
-    }
-
 }
--- src/main/java/org/example/domain/Person.java
+++ src/main/java/org/example/domain/Person.java
@@ -1,4 +1,37 @@
 package org.example.domain;
 
 public class Person {
+    private String name;
+    private int age;
+    private String role;
+    private String use;
+
+    private void setRole(){
+
+    }
+    private void setUse(){
+
+    }
+    private void setName(){
+
+    }
+    private void setAge(){
+
+    }
+    private String getName(){
+        this.name = name;
+        return "";
+    }
+    private String getUse(){
+        this.use = use;
+        return "";
+    }
+    private String getRole(){
+        this.role = role;
+        return "";
+    }
+    private int getAge(){
+        this.age = age;
+        return 0;
+    }
 }
